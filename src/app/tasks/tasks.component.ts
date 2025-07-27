1bed096646688aba4471ab1b96cddf5fb5ec7247
David L. Batton <battondl@gmail.com>
2025-07-27 12:18:37 -0500

    add open dialog and cancel dialog

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { AddTaskComponent } from "./add-task/add-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+onAddTask($event: Event) {
+throw new Error('Method not implemented.');
+}
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name?: string;
+
+  isAddingTask = false;
+
   tasks = [
     {
       id: 't1',
@@ -43,5 +50,13 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
