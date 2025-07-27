f838f7cc4e1ece117e4f62af52e55609d3f21efd
David L. Batton <battondl@gmail.com>
2025-07-27 09:00:22 -0500

    add multiple users config

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,6 @@
-import { Component, computed, signal } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
-import { DUMMY_USERS } from "../dummy-users";
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,11 +9,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrls: ['./user.component.css'], 
 })
 export class UserComponent {
-    selectedUser = signal(DUMMY_USERS[randomIndex]) ;
-    imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
+  get imagePath() { 
+    return 'assets/users/' + this.avatar;
+
+  }
     onSelectUser() {
-        const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-        this.selectedUser.set(DUMMY_USERS[randomIndex]);
+        console.log('User selected');
     }
 }
